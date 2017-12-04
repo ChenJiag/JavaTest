c5d0fd9e11c1f4de61a11b0c19fd60a17dabccda
LoveJOJO <1340266298@qq.com>
2017-12-04 19:15:31 +0800

    Update StringBufferAndStringBuilder.java

--- StringBufferAndStringBuilder.java
+++ StringBufferAndStringBuilder.java
@@ -1,3 +1,28 @@
 首先StringBuffer和StringBuilder都是用char[] 存储的，当存储到了一定程度需要扩大的时候，OpenJdk为其分配两倍的新空间。
 
 char[] value;
+int count;
+int length = str.lengtn();
+public AbstractStringBuilder append(String str) {
+    if(str == null)
+        str = "null";
+    if(len == 0)
+        return this;
+    int newCount = len + count;
+    if(newCount > value.length)
+        expandCapacity(newCount);
+    str.getChars(0, len, value, count);
+    count = newCount;
+    return this;
+}
+void expandCapacity(int minimunCapacity) {
+    int newCapacity = (value.length + 1) * 2;
+    if(newCapacity < 0） {
+        newCapacity = Integer.MAX_VALUE;
+    } else if (minimunCapacity > newCapacity) {
+        newCapacity = miniumCapacity;
+      }
+    value = Array.copyOf(value,newCapacity);
+}
+    
+  
